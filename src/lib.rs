e3fba89dd1653207a121934468ff32a3c0632c0b
Luc Street <6709262+0xSiO@users.noreply.github.com>
2020-10-07 16:17:02 -0700

    Add osdev wiki links to references

--- src/lib.rs
+++ src/lib.rs
@@ -1,6 +1,10 @@
 #![no_std]
 
 // References:
+//   https://wiki.osdev.org/%228042%22_PS/2_Controller
+//   https://wiki.osdev.org/PS2_Keyboard
+//   https://wiki.osdev.org/PS/2_Mouse
+//   https://wiki.osdev.org/Mouse_Input
 //   https://www.avrfreaks.net/sites/default/files/PS2%20Keyboard.pdf
 //   http://www.s100computers.com/My%20System%20Pages/MSDOS%20Board/PC%20Keyboard.pdf
 
