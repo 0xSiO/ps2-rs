66f31e854a7e3c6bc468769fae1abc621e164f5b
Luc Street <lucis-fluxum@users.noreply.github.com>
2020-10-07 19:42:44 -0700

    Clean up module headers and constants

--- src/lib.rs
+++ src/lib.rs
@@ -8,9 +8,14 @@
 //   https://www.avrfreaks.net/sites/default/files/PS2%20Keyboard.pdf
 //   http://www.s100computers.com/My%20System%20Pages/MSDOS%20Board/PC%20Keyboard.pdf
 
+pub use self::controller::Controller;
+
 mod controller;
-pub mod error;
-pub mod flags;
 mod response;
 
-pub use self::controller::Controller;
+pub mod error;
+pub mod flags;
+
+const DATA_PORT: u16 = 0x60;
+const COMMAND_REGISTER: u16 = 0x64;
+const TIMEOUT: u16 = 10_000;
